b42e3c1b0722baf1eae09a2bd2c3c981f621f079
zmy <825386224@qq.com>
2018-05-20 00:42:01 +0800

    新增：2.0版本

--- app/src/main/java/cn/zmy/statusbatdragdemo/MainActivity.java
+++ app/src/main/java/cn/zmy/statusbatdragdemo/MainActivity.java
@@ -1,5 +1,8 @@
 package cn.zmy.statusbatdragdemo;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,13 +12,17 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.WindowManager;
+import android.widget.ViewAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
     private WindowManager mWindowManager;
+    WindowManager.LayoutParams mLayoutParams;
     private GestureDetector mGestureDetector;
     private View mView;
+    private int mViewHeightWhenInVisible = 120;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,29 +38,134 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("SCROLL", distanceY + "");
                 float targetTranslationY = Math.min(0, mView.getTranslationY() - distanceY);
                 mView.setTranslationY(targetTranslationY);
+                updateBackgroundDim();
                 return true;
             }
         });
         mView = LayoutInflater.from(this).inflate(R.layout.layout_fuc, null);
-        mView.measure(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
-        mView.setTranslationY(-mView.getMeasuredHeight());
+        mView.setTranslationY(-mViewHeightWhenInVisible
+        );
         mView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
+                if (!v.isEnabled()) {
+                    return false;
+                }
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (mLayoutParams.height != WindowManager.LayoutParams.WRAP_CONTENT) {
+                        mLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+                        mView.measure(mLayoutParams.width, mLayoutParams.height);
+                        mWindowManager.updateViewLayout(mView, mLayoutParams);
+                        mView.setTranslationY(-mView.getMeasuredHeight());
+                        updateBackgroundDim();
+                    }
+                }
                 mGestureDetector.onTouchEvent(event);
-                return false;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (Math.abs(mView.getTranslationY()) <= mView.getMeasuredHeight() / 2) {
+                        //下拉超过临界距离
+                        mView.animate()
+                                .translationY(0)
+                                .setDuration(300)
+                                .setListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animation) {
+                                        mView.setEnabled(false);
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        mView.setEnabled(true);
+                                        updateBackgroundDim();
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animation) {
+                                        mView.setEnabled(true);
+                                        updateBackgroundDim();
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animation) {
+
+                                    }
+                                })
+                                .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                    @Override
+                                    public void onAnimationUpdate(ValueAnimator animation) {
+                                        updateBackgroundDim();
+                                    }
+                                })
+                                .start();
+                        mLayoutParams.flags &= ~WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+                        mWindowManager.updateViewLayout(mView, mLayoutParams);
+                    } else {
+                        mView.animate()
+                                .translationY(-mView.getMeasuredHeight())
+                                .setDuration(300)
+                                .setListener(new Animator.AnimatorListener() {
+                                    @Override
+                                    public void onAnimationStart(Animator animation) {
+                                        mView.setEnabled(false);
+                                    }
+
+                                    @Override
+                                    public void onAnimationEnd(Animator animation) {
+                                        mView.setEnabled(true);
+                                        mLayoutParams.height = mViewHeightWhenInVisible;
+                                        mView.setTranslationY(-mViewHeightWhenInVisible);
+                                        mLayoutParams.dimAmount = 0f;
+                                        mWindowManager.updateViewLayout(mView, mLayoutParams);
+                                    }
+
+                                    @Override
+                                    public void onAnimationCancel(Animator animation) {
+                                        mView.setEnabled(true);
+                                        mLayoutParams.height = mViewHeightWhenInVisible;
+                                        mView.setTranslationY(-mViewHeightWhenInVisible);
+                                        mWindowManager.updateViewLayout(mView, mLayoutParams);
+                                        updateBackgroundDim();
+                                    }
+
+                                    @Override
+                                    public void onAnimationRepeat(Animator animation) {
+
+                                    }
+                                })
+                                .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                                    @Override
+                                    public void onAnimationUpdate(ValueAnimator animation) {
+                                        updateBackgroundDim();
+                                    }
+                                })
+                                .start();
+                        mLayoutParams.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+                        mWindowManager.updateViewLayout(mView, mLayoutParams);
+                    }
+                }
+                return true;
             }
         });
-        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(
+        mLayoutParams = new WindowManager.LayoutParams(
                 WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.WRAP_CONTENT,
+                mViewHeightWhenInVisible,
                 // Allows the view to be on top of the StatusBar
                 WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
                 // Draws over status bar
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_DIM_BEHIND,
                 PixelFormat.TRANSPARENT);
-        layoutParams.gravity = Gravity.TOP;
-        layoutParams.dimAmount = 0.1f;
-        mWindowManager.addView(mView, layoutParams);
+        mLayoutParams.gravity = Gravity.TOP;
+        mLayoutParams.dimAmount = 0f;
+        mWindowManager.addView(mView, mLayoutParams);
+    }
+
+    private void updateBackgroundDim() {
+        float currentTranslationY = mView.getTranslationY();
+        //TranslationY 0 -> dim 0.5f
+        //TranslationY -mView.height -> dim 0f
+        float dim = currentTranslationY / mView.getMeasuredHeight() / 2 + 0.5f;
+        mLayoutParams.dimAmount = dim;
+        Log.d("dim", "" + dim);
+        mWindowManager.updateViewLayout(mView, mLayoutParams);
     }
 }
